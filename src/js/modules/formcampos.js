0eb8db73bc2c87e1345669fd9686cdd1e0121b48
Jhon Costa <jhon.costa.dev@gmail.com>
2024-02-05 21:24:08 -0300

    refactor(lógica): métodos da classe Formadicionar refatorados para maior entendimento

--- src/js/modules/formcampos.js
+++ src/js/modules/formcampos.js
@@ -1,16 +1,53 @@
 export class Formadicionar{
     constructor(){
-        this.recuperarDados()
+        this.configurarListeners()
         this.listaPreVisualizacao = JSON.parse(localStorage.getItem('previsualizacao')) || []
     }
 
     previsualizacaoLocalStorage(){
         localStorage.setItem('previsualizacao', JSON.stringify(this.listaPreVisualizacao));
     }
-    recuperarDados(){
+
+    validarCampos(){
+        return (
+            this.nomeProduto !== "" &&
+            this.validade !== "" &&
+            this.fabricante !== "" &&
+            this.quantidade !== "" &&
+            this.grupo !== ""
+        )
+    }
+
+
+    adicionarProdutos(){
+        const produto = {
+            nome: this.nomeProduto,
+            validade: this.validade,
+            fabricante: this.fabricante,
+            quantidade: this.quantidade,
+            grupo: this.grupo,
+        }
+        this.listaPreVisualizacao.push(produto)
+        this.previsualizacaoLocalStorage()
+        this.div.remove()
+    }
+
+
+    exibirPreencherTodos() {
+        this.seletormodal = document.querySelector(".h4-modalcampos")
+        if(!this.seletormodal){
+            this.div = document.createElement("h4")
+            this.div.textContent = "Preencha todos os campos!"
+            this.div.classList.add("h4-modalcampos")
+            this.h1Titulo.parentNode.insertBefore(this.div, this.h1Titulo.nextSibling)
+        }
+    }
+
+    configurarListeners() {
         this.btnAdc = document.querySelector("#btn-adc")
-        this.btnAdc.addEventListener("click", (e)=>{
+        this.btnAdc.addEventListener("click", (e) => {
             e.preventDefault()
+
             this.nomeProduto = document.querySelector("#nome-produto").value.trim()
             this.validade = document.querySelector("#validade").value.trim()
             this.fabricante = document.querySelector("#fabricante").value.trim()
@@ -18,32 +55,11 @@ export class Formadicionar{
             this.grupo = document.querySelector("#grupo-produto").value.trim()
             this.h1Titulo = document.querySelector(".h1-cadastroproduto")
 
-            if(
-                this.nomeProduto !== "" &&
-                this.validade !== "" &&
-                this.fabricante !== "" &&
-                this.quantidade !== "" &&
-                this.grupo !== ""
-            ){
-                const produto = {
-                    nome: this.nomeProduto,
-                    validade: this.validade,
-                    fabricante: this.fabricante,
-                    quantidade: this.quantidade,
-                    grupo: this.grupo,
-                }
-                this.listaPreVisualizacao.push(produto)
-                this.previsualizacaoLocalStorage()
+            if (this.validarCampos()) {
+                this.adicionarProdutos()
             } else {
-                let seletormodal = document.querySelector(".h4-modalcampos")
-                if(!seletormodal){
-                    let div = document.createElement("h4")
-                    div.textContent = "Preencha todos os campos!"
-                    div.classList.add("h4-modalcampos")
-                    this.h1Titulo.parentNode.insertBefore(div, this.h1Titulo.nextSibling)
-                }
+                this.exibirPreencherTodos()
             }
         })
     }
 }
-
